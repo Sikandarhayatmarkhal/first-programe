6a5b8af6164bf63e722698d6011f10c788fb1823
Sikandarhayatmarkhal <sikandar1071@gmail.com>
2024-08-08 18:54:22 +0500

    Update main.py
    this is updated version of code

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-print("hello world")
-print("dg
-")
+def sum():
+    return a+b
+def minus():
+    return a-b
+def multiply():
+    return a*b
+def divide():
+    return a/b
+def operatorcheck(a,b):
+    # operator=input("select any arithematic operator to perform the operation:\n")
+    if operator=="+":
+       print(f"the sum is {sum()}")
+    if operator=="-":
+        print(f"the minus of both number is {minus()}")
+    if operator=="*":
+        print(f"the multiplication of both number give us {multiply()}")
+    if operator=="/":
+        print(f"the division  of both number give us  {divide()}")
+a=int(input("enter first number here:\n"))
+operator=input('''select any arithematic operator to perform the operation:''')
+b=int(input("enter your second here:\n"))
+operatorcheck(a,b)
